8999ad5fef01619fac51ef68ac40fc8ad3a1a208
Joe Vinten <joseph.vinten@gmail.com>
2025-08-26 13:57:26 +0100

    refactor: update catch logic

--- command_catch.go
+++ command_catch.go
@@ -22,14 +22,15 @@ func commandCatch(cfg *config, args []string) error {
 		return err
 	}
 
-	catchRoll := rand.Intn(320) + 1
+	catchRoll := rand.Intn(pokemonDetails.BaseExperience)
 
-	if catchRoll > pokemonDetails.BaseExperience {
-		fmt.Printf("%s was caught\n", pokemonName)
-		cfg.pokedex[pokemonName] = pokemonDetails
-	} else {
+	if catchRoll > 40 {
 		fmt.Printf("%s escaped\n", pokemonName)
+		return nil
 	}
+
+	fmt.Printf("%s was caught\n", pokemonName)
+	cfg.pokedex[pokemonName] = pokemonDetails
 
 	return nil
 }
